71b00495b943fda5cfda8e7d3042e96db41832c8
TheLegendLord <fabcic009@gmail.com>
2026-01-09 16:02:06 +0100

    added for angular

--- src/main/java/com/sportly/api_gateway/security/SecurityConfig.java
+++ src/main/java/com/sportly/api_gateway/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import javax.crypto.spec.SecretKeySpec;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
@@ -23,6 +24,7 @@ public class SecurityConfig {
         return http
             .csrf(ServerHttpSecurity.CsrfSpec::disable)
             .authorizeExchange(exchanges -> exchanges
+                .pathMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                 .pathMatchers("/api/auth/**").permitAll()
                 .pathMatchers(
                     "/api/charts/**",
